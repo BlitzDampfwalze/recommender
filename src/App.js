228dfb6e2fdcb544ab9c3c7e61db80d2b5b58c4e
Paul Lime <paullime@gmail.com>
2019-05-13 01:38:57 -0500

    initial component structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import './App.css';
 
+import firebase from './firebase';
+import Dashboard from './components/Dashboard';
+import Homepage from './components/HomePage';
+import Login from './components/Login';
+import Register from './components/Register';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Recommender</h1>
-      </header>
-    </div>
-  );
+  const [firebaseInitialized, setFirebaseInitialized] = useState(false);
+
+  useEffect(()=> {
+    firebase.isInitialized().then(val=>{
+      setFirebaseInitialized(val);
+    });
+  });
+
+  return firebaseInitialized !== false ? (
+    <main className="App">
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/dashboard' component={Dashboard} />
+        </Switch>
+      </Router>
+    </main>
+  ) : (
+    <header className="App-header">
+    <h1>Recommender</h1>
+    <h2>Loading...</h2>
+    </header>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import firebase from '../firebase';
+
+const Navbar = (props) => {
+  return (
+    <div>
+      recommender
+    </div>
+  );
+};
+
+export default Navbar;
