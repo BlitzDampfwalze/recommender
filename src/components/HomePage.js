9a3e45624396c8165a2398b9091170c8d37807ec
Paul Lime <paullime@gmail.com>
2019-05-14 02:28:59 -0500

    added navbar, register, login functionality

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Navbar from './Navbar';
 
-const Homepage = () => {
-  return <div>Homepage</div>
+const Homepage = (props) => {
+  return (
+    <main>
+      <Navbar {...props} />
+      <div className="container">
+        <h2>Recommender Homepage</h2>
+        <p>lorem ipsum</p>
+      </div>
+    </main>
+  )
 };
 
 export default Homepage;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import firebase from '../firebase';
 
-const Register = () => {
-  return <div>register</div>
+const Register = (props) => {
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  async function onRegister() {
+    try {
+      await firebase.register(name, email, password);
+      props.history.replace('/dashboard')
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+
+  return (
+    <main>
+      <Navbar {...props} />
+      <div className="container">
+        <h2>
+          Recommender Register
+        </h2>
+
+        <form onSubmit={e => e.preventDefault() && false}>
+
+          <div className="input-field">
+            <input
+              placeholder="username"
+              type="text" value={name}
+              aria-label="username"
+              onChange={e => setName(e.target.value)}
+            />
+          </div>
+
+          <div className="input-field">
+            <input
+              placeholder="email"
+              type="text" value={email}
+              aria-label="email"
+              onChange={e => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="input-field">
+            <input
+              placeholder="password"
+              type="text" value={password}
+              aria-label="password"
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button 
+          type="submit"
+          className="button is-primary"
+          onClick={onRegister}
+          >
+          Register
+          </button>
+
+        </form>
+      </div>
+    </main>
+  )
 };
 
 export default Register;
